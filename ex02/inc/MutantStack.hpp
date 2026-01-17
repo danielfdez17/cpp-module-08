0704e548d3d02ddc97f97da48f2661486f13e1ca
danielfdez17 <daniel17dev@gmail.com>
2026-01-17 09:12:41 +0100

    feat: implement std::stack<T> iterators

--- ex02/inc/MutantStack.hpp
+++ ex02/inc/MutantStack.hpp
@@ -13,23 +13,19 @@ class MutantStack : public std::stack<T>
 private:
 
 public:
-	// MutantStack();
-	// MutantStack(MutantStack const& copy);
-	// MutantStack &operator=(MutantStack const& copy);
-	// ~MutantStack();
-	// typename MutantStack<T>::iterator begin () { return this->c.begin(); }
-	// typename MutantStack<T>::iterator end () { return this->c.end(); }
+	MutantStack();
+	MutantStack(MutantStack const& copy);
+	MutantStack &operator=(MutantStack const& copy);
+	~MutantStack();
 
-    using container_type = typename std::stack<T>::container_type;
-    using iterator = typename container_type::iterator;
-    using const_iterator = typename container_type::const_iterator;
-
-    iterator begin() { return this->c.begin(); }
-    iterator end()   { return this->c.end(); }
-
-    const_iterator begin() const { return this->c.begin(); }
-    const_iterator end()   const { return this->c.end(); }
-	
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
+	const_iterator begin() const { return this->c.begin(); }
+	const_iterator end() const { return this->c.end(); }
 };
+
+#include "MutantStack.tpp"
 
 #endif // __MUTANTSTACK_HPP__
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,31 +1,62 @@
 #include "MutantStack.hpp"
 #include "colors.hpp"
 #include <iostream>
-#include <exception>
-#include <stdexcept>
+#include <list>
 
 int main()
 {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		std::cout << YELLOW << "----- MutantStack Test -----\n" RESET;
+		MutantStack<int> mstack;
+		std::cout << "is mstack empty? " << (mstack.empty() ? "yes" : "no") << "\n";
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << "top: " << mstack.top() << "\n";
+		mstack.pop();
+		std::cout << "size: " << mstack.size() << "\n";
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+		std::cout << "is mstack empty? " << (mstack.empty() ? "yes" : "no") << "\n";
+		MutantStack<int>::const_iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
 		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << "\n";
+			++it;
+		}
+		MutantStack<std::string> mstack_str;  // ! default constructor
+		std::stack<int> s(mstack);			  // ! copy constructor
+		MutantStack<int> mstack_int = mstack; // ! assignment operator
 	}
-	std::stack<int> s(mstack);
+	{
+		std::cout << YELLOW << "----- List Test Comparison -----\n" RESET;
+		std::list<int> list;
+		std::cout << "is list empty? " << (list.empty() ? "yes" : "no") << "\n";
+		list.push_back(5);
+		list.push_back(17);
+		std::cout << "top(back): " << list.back() << "\n";
+		list.pop_back();
+		std::cout << "size: " << list.size() << "\n";
+		list.push_back(3);
+		list.push_back(5);
+		list.push_back(737);
+		//[...]
+		list.push_back(0);
+		std::cout << "is list empty? " << (list.empty() ? "yes" : "no") << "\n";
+		std::list<int>::const_iterator it = list.begin();
+		std::list<int>::iterator ite = list.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << "\n";
+			++it;
+		}
+	}
 	return 0;
 }
